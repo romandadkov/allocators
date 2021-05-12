f6bd940665023486f719da9da5ffdb9c91ee33d9
Roman Dadkov <r.dadkov@gmail.com>
2021-05-12 18:18:28 +0300

    memory_pool_based_allocator
    * memory_pool implementation
    ** initialize
    ** is_defined
    ** allocate
    ** deallocate

--- memory_pool_based_allocator/src/memory_pool.h
+++ memory_pool_based_allocator/src/memory_pool.h
@@ -73,4 +73,81 @@ namespace memory_pool
         return get_instance<id>(std::make_index_sequence<bucket_count<id>>());
     }
 
+    struct info
+    {
+        std::size_t index{0};       // which bucket?
+        std::size_t block_count{0}; // how many blocks would the allocation take from the bucket?
+        std::size_t waste{0};       // how much memory would be wasted?
+
+        bool operator<(const info &other) const noexcept
+        {
+            return (waste == other.waste) ? block_count < other.block_count : waste < other.waste;
+        }
+    };
+
+    template <std::size_t id>
+    [[nodiscard]] void *allocate(std::size_t bytes)
+    {
+        auto &pool = get_instance<id>();
+        std::array<info, bucket_count<id>> deltas;
+        std::size_t index = 0;
+        for (const auto &bucket : pool)
+        {
+            deltas[index].index = index;
+            if (bucket.BlockSize >= bytes)
+            {
+                deltas[index].waste = bucket.BlockSize - bytes;
+                deltas[index].block_count = 1;
+            }
+            else
+            {
+                const auto n = 1 + ((bytes - 1) / bucket.BlockSize);
+                const auto storage_required = n * bucket.BlockSize;
+                deltas[index].waste = storage_required - bytes;
+                deltas[index].block_count = n;
+            }
+            ++index;
+        }
+
+        sort(deltas.begin(), deltas.end()); // std:::sort is allowed to allocate
+
+        for (const auto &d : deltas)
+        {
+            if (auto ptr = pool[d.index].allocate(bytes); ptr != nullptr)
+            {
+                return ptr;
+            }
+        }
+
+        throw std::bad_alloc{};
+    }
+
+    template <std::size_t id>
+    void deallocate(void *ptr, std::size_t bytes)
+    {
+        auto &pool = get_instance<id>();
+
+        for (const auto &bucket : pool)
+        {
+            if (bucket.belongs(ptr))
+            {
+                bucket.deallocate(ptr, bytes);
+                return;
+            }
+        }
+    }
+
+    template <std::size_t id>
+    constexpr bool is_defined() noexcept
+    {
+        return bucket_count<id> != 0;
+    }
+
+    template <std::size_t id>
+    bool initialize() noexcept
+    {
+        (void)get_instance<id>();
+        return is_defined<id>();
+    }
+
 } // namespace memory_pool
